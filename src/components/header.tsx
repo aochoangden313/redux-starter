d9f92913badfcca41fef50de85f058b1dec9445b
quanlv.56 <quanlv.56@gmail.com>
2025-05-29 09:52:00 +0700

    lesson 39: dark/light mode

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,34 +2,39 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import { useAppSelector } from '../redux/hook';
+import { useAppDispatch, useAppSelector } from '../redux/hook';
+import { Form } from 'react-bootstrap';
+import { useEffect, useState } from 'react';
+import { changeMode } from '../redux/app/app.slide';
 
 function Header() {
 
-   const users = useAppSelector((state) => state.user.listUsers);
+  const users = useAppSelector((state) => state.user.listUsers);
+  // const [mode, setMode] = useState('light');
+  const mode = useAppSelector((state) => state.app.mode);
+  const dispatch = useAppDispatch();
 
+  useEffect(() => {
+    const body = document.querySelector('body');
+    if (body) body.setAttribute('data-bs-theme', mode);
+  }, [mode]);
 
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar expand="lg" className="bg-body-tertiary" data-bs-theme={mode}>
       <Container>
         <Navbar.Brand href="#home">QuanLV Navbar {users.length}</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
+        <Navbar.Collapse id="basic-navbar-nav" className='justify-content-end'>
+          <Form.Check // prettier-ignore
+            type="switch"
+            id="custom-switch"
+            value={mode}
+            onChange={(e) => dispatch(changeMode(e.target.value === 'light' ? 'dark' : 'light'))}
+            label={mode === 'light' ?
+              <Navbar.Text>Light Mode</Navbar.Text>
+              : <Navbar.Text>Dark Mode</Navbar.Text>
+            }
+          />
         </Navbar.Collapse>
       </Container>
     </Navbar>
